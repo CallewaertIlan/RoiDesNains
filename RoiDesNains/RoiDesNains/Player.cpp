b7225b7d585ce3ba811afc8c1140bb2a2fbc5bc5
unknown <ylemoine@gamingcampus.local>
2022-10-31 13:00:55 +0100

    add files

--- RoiDesNains/RoiDesNains/Player.cpp
+++ RoiDesNains/RoiDesNains/Player.cpp
@@ -1,1 +1,16 @@
 #include "Framework.h"
+void Player::OnEnter(int oldState)
+{
+}
+
+void Player::OnExecute()
+{
+}
+
+void Player::OnExit(int newState)
+{
+}
+
+void Player::OnUpdate() {
+
+}
--- RoiDesNains/RoiDesNains/Player.h
+++ RoiDesNains/RoiDesNains/Player.h
@@ -1,1 +1,23 @@
 #pragma once
+class Player
+{
+public:
+	Player();
+	virtual ~Player();
+
+	virtual void OnEnter(int oldState);
+	virtual void OnExecute();
+	virtual void OnExit(int newState);
+	virtual void OnUpdate();
+
+private:
+
+};
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
