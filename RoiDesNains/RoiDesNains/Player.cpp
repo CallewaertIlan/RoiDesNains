08f843e4d9e5d7fa25c3df79a040606304d9c96a
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2022-11-07 11:27:05 +0100

    add collision with ground

--- RoiDesNains/RoiDesNains/Player.cpp
+++ RoiDesNains/RoiDesNains/Player.cpp
@@ -3,8 +3,8 @@
 Player::Player(){
 	PlayerTexture.loadFromFile("../Images/minersprite.png");
 	PlayerSprite.setTexture(PlayerTexture);
-	setScale(1.95, 1.95);
-	setPosition(90, 60);
+	PlayerSprite.setScale(1.95, 1.95);
+	PlayerSprite.setPosition(90, 60);
 	
 }
 
@@ -27,59 +27,32 @@ void Player::OnExit(int newState)
 void Player::OnUpdate(vector<sf::RectangleShape> listEntities)
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-		bool collide = false;
-		for (int i = 0; i < listEntities.size(); i++)
-		{
-			collide = Collision(listEntities[i], "right");
-		}
-		if (!collide) {
-			PlayerSprite.move(sf::Vector2f(1.2f, 0.0f));
-		};
+		PlayerSprite.move(sf::Vector2f(1.2f, 0.0f));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-		bool collide = false;
-		for (int i = 0; i < listEntities.size(); i++)
-		{
-			collide = Collision(listEntities[i], "left");
-		}
-		if (!collide) {
-			PlayerSprite.move(sf::Vector2f(-1.2f, 0.0f));
-		};
+		PlayerSprite.move(sf::Vector2f(-1.2f, 0.0f));
 	}
-	//PlayerSprite.move(sf::Vector2f(0.0f, 2.0f));
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+		PlayerSprite.move(sf::Vector2f(0.0f, -5.0f));
+	}
+	bool collide = false;
+	for (int i = 0; i < listEntities.size(); i++)
+	{
+		if (Collision(listEntities[i], "down") == true)
+			collide = true;
+	}
+	if (!collide) {
+		PlayerSprite.move(sf::Vector2f(0.0f, 3.0f));
+	};
+	
 }
 
 bool Player::Collision(sf::RectangleShape entity, string move_type)
 {
-	cout << "Entity x" << entity.getPosition().x << endl;
-	cout << "Player x" << PlayerSprite.getPosition().x << endl;
-	
-	cout << endl;
-	cout << "Entity y" << entity.getPosition().y << endl;
-	cout << "Player y" << PlayerSprite.getPosition().y << endl;
-	cout << endl;
-	cout << endl;
+	//if (move_type == "down")
+	//	return getSprite().getGlobalBounds().intersects(entity.getGlobalBounds());
+	//else if (move_type == "right")
+	//	return getSprite().getGlobalBounds().intersects(entity.getGlobalBounds());
+	return getSprite().getGlobalBounds().intersects(entity.getGlobalBounds());
 
-	if (move_type == "right") {
-		if (entity.getPosition().x + SIZE_X_SPRITE_PLAYER > entity.getPosition().x && entity.getPosition().x + SIZE_X_SPRITE_PLAYER < entity.getPosition().x + SIZE_X_SPRITE_CUBE) {
-			if (entity.getPosition().y > entity.getPosition().y && entity.getPosition().y < entity.getPosition().y + SIZE_Y_SPRITE_CUBE) {
-				return true;
-			}
-		}
-	}
-	if (move_type == "left") {
-		if (entity.getPosition().x > entity.getPosition().x && entity.getPosition().x < entity.getPosition().x + SIZE_X_SPRITE_CUBE) {
-			if (entity.getPosition().y > entity.getPosition().y && entity.getPosition().y < entity.getPosition().y + SIZE_Y_SPRITE_CUBE) {
-				return true;
-			}
-		}
-	}
-	return false;
-	//if (move_type == "up") {
-	//	if (entity.getPosition().x > entity.getPosition().x && entity.getPosition().x < entity.getPosition().x + SIZE_X_SPRITE_CUBE) {
-	//		if (entity.getPosition().y > entity.getPosition().y && entity.getPosition().y < entity.getPosition().y + SIZE_Y_SPRITE_CUBE) {
-	//			return false;
-	//		}
-	//	}
-	//}
 }
