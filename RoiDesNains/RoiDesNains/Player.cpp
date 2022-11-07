1cd1cece82107b09538b27ddff6f757feb0e8760
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2022-11-07 17:01:40 +0100

    add sf view

--- RoiDesNains/RoiDesNains/Player.cpp
+++ RoiDesNains/RoiDesNains/Player.cpp
@@ -4,8 +4,11 @@ Player::Player(){
 	PlayerTexture.loadFromFile("../Images/testSpriteSheet.png");
 	PlayerTexture.setSmooth(FALSE);
 	PlayerSprite.setTexture(PlayerTexture);
-	PlayerSprite.setScale(1.95f, 1.95f);
-	PlayerSprite.setPosition(90, 60);
+	PlayerSprite.setScale(3.9f, 3.9f);
+	PlayerSprite.setPosition(180, 150);
+	m_view.setSize(1200, 775);
+
+	PlayerSprite.setOrigin(SIZE_X_SPRITE_PLAYER / 2.0f, SIZE_Y_SPRITE_PLAYER / 2.0f);
 	
 	m_anim.initiaisation(&PlayerTexture, sf::Vector2u(5, 1), 0.3f);
 }
@@ -28,15 +31,16 @@ void Player::OnExit(int newState)
 
 void Player::OnUpdate(vector<sf::RectangleShape>& listEntities, int row, float deltaTime)
 {
+	m_view.setCenter(PlayerSprite.getPosition().x, PlayerSprite.getPosition().y);
 	m_anim.Update(row, deltaTime);
 	PlayerSprite.setTextureRect(m_anim.getUvRect());
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-		PlayerSprite.setScale(1.95f, 1.95f);
-		PlayerSprite.move(sf::Vector2f(1.2f, 0.0f));
+		PlayerSprite.setScale(3.9f, 3.9f);
+		PlayerSprite.move(sf::Vector2f(2.2f, 0.0f));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-		PlayerSprite.setScale(-1.95f, 1.95f);
-		PlayerSprite.move(sf::Vector2f(-1.2f, 0.0f));
+		PlayerSprite.setScale(-3.9f, 3.9f);
+		PlayerSprite.move(sf::Vector2f(-2.2f, 0.0f));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
 		PlayerSprite.move(sf::Vector2f(0.0f, -5.0f));
